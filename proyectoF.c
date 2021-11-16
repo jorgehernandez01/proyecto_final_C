bff1b243ef11c3143c417bcf185178184d110f70
JLCC2003 <91859345+JLCC2003@users.noreply.github.com>
2021-11-16 16:09:33 -0600

    Update proyectoF.c

--- proyectoF.c
+++ proyectoF.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+
 #include <time.h>
 
 int main(){
     //variables para los ciclos
     int i, m;
-    //variables para almacenar los valores de dia, mes y aÒo de la estructura tm
-    int dia, mes, anio;
+    //variables para almacenar los valores de dia, mes y aÔøΩo de la estructura tm
+    int dia, mes, anio = 2005 ;
     //variable que almacena el tiempo del equipo.
     time_t sisTime;
     //varible puntero para poder acceder a los valores de la estructura 'tm'
     struct tm *fActual;
 
-    time(&sisTime); //asignamos la fecha actual a la variable que almacenar· el tiempo del equipo por medio de la fucion 'time()'
+    time(&sisTime); //asignamos la fecha actual a la variable que almacenarÔøΩ el tiempo del equipo por medio de la fucion 'time()'
      // convertimos los valores almacenados en la variable 'sisTime' a compatibles con la estructura 'tm' y lo asignamos al puntero
     fActual = localtime(&sisTime);
     //impresion de la fecha, accediendo a los campos de la estructura 'tm' (temporal)
     printf("Hoy es: %i/%i/%i", fActual->tm_mday, fActual->tm_mon, 1900+fActual->tm_year);
 
-    return 0;
+if (anio % 4 != 0 || (anio % 100 == 0 && anio % 400 !=0)) // Dado que un a√±o bisiesto es divisible entre 4 usamos la siguiente formula 
+  printf("\nNo es  a√±o bisiesto");
+  else
+  printf("\nEs a√±o bisiesto");
+  printf("\n\n\n"); 
+  return 0;
+    
+    
 }
