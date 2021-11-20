c9a7e82c972dccb0f049780a3b6a8af641a38a35
jorgehernandez01 <93957851+jorgehernandez01@users.noreply.github.com>
2021-11-19 18:26:31 -0600

    eliminacion de codigo comentado
    lineas 161-175

--- proyectoF.c
+++ proyectoF.c
@@ -158,26 +158,12 @@ void calendario(int dia, int mes, int anio){
 			scanf("%d",&m);
 			printf("Introduce un anyo en numero: ");
 			scanf("%d",&a);
-
-		/*	if((a%4==0) && !(a%100==0))
-			{
-    			m=bisiesto[m-1];
-			}
-			else if (a%400==0)
-			{
-    			m=bisiesto[m-1];
-			}
-			else
-			{
-    			m=regular[m-1];
-			}
-        */
-result1=(a-1)%7;
-result2=(a-1)/4;
-result3=(3*(((a-1)/100)+1))/4;
-result4=(result2-result3)%7;
-result5=d%7;
-d=(result1+result4+m+result5)%7;
+            result1=(a-1)%7;
+            result2=(a-1)/4;
+            result3=(3*(((a-1)/100)+1))/4;
+            result4=(result2-result3)%7;
+            result5=d%7;
+            d=(result1+result4+m+result5)%7;
 			switch(d)
 			{
 				case 1:
@@ -205,7 +191,7 @@ d=(result1+result4+m+result5)%7;
 			system("pause");
       break;
 
-        
+
       case 4:
         system("exit");
         break;
